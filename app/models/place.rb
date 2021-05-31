303a4948815c7d4ebee0653fd88833a7a95b98ec
andrea furthmair <a.furthmair@gmail.com>
2021-05-31 17:59:34 +0200

    validations done

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,4 +6,7 @@ class Place < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :sport_combinations
   has_many :sport_types, through: :sport_combinations
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10, message: "Description has to be at least 10 characters" }
+  validates :adress, presence: true
 end
--- app/models/sport_type.rb
+++ app/models/sport_type.rb
@@ -1,4 +1,5 @@
 class SportType < ApplicationRecord
   has_many :sport_combinations
   has_many :places, through: :sport_combinations
+  validates :name, presence: true
 end
