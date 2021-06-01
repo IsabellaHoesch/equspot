d01545e7a1f7eca2bf289e88185d71bdfad66e4b
Armen Kaltak <arkouo@gmail.com>
2021-06-01 16:22:05 +0200

    Generate and defined the index and create methods for favourites. Add the policies. Made the index page for favourites. Added a link at the show page(places) to add them to the FavList.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,26 @@
 class FavouritesController < ApplicationController
+  def index
+    # @place = Place.find(params[:place_id])
+    @favourites = policy_scope(Favourite)
+  end
+
+  def create
+    @user = current_user
+    @place = Place.find(params[:place_id])
+    @favourite = Favourite.new
+    @favourite.user = @user
+    @favourite.place = @place
+    authorize @favourite
+    if @favourite.save
+      redirect_to places_path, notice: "Added to the fav."
+    else
+      render "places/show"
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:favourite).permit(:place_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :places, only: [ :index, :new, :create, :show, :edit, :update ] do
-    resources :favourites, only: [ :index, :create]
+    resources :favourites, only: [ :create ]
   end
-
+  resources :favourites, only: [ :index ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
