b9e332f69b8b0e7d1b76ce7dc3321a98c3302607
Armen Kaltak <arkouo@gmail.com>
2021-06-01 14:47:49 +0200

    Configurate the routes for the favourite controller and generate the fav controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :places, only: [ :index, :new, :create, :show, :edit, :update ]
+  resources :places, only: [ :index, :new, :create, :show, :edit, :update ] do
+    resources :favourites, only: [ :index, :create]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
