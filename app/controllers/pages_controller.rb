cc7e5aac9d6fbf36790b54aed22ac2ae63215e72
andrea furthmair <a.furthmair@gmail.com>
2021-06-01 17:14:40 +0200

    added right map code to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @places = policy_scope(Place)
+
+    @markers = Place.geocoded.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude,
+        info_window: render_to_string(partial: "places/info_window", locals: { place: place }),
+        image_url: helpers.asset_url('EquRent.png')
+      }
+    end
   end
 end
