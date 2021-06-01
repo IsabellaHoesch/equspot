70f0b23b33ccb0c415ed278b61569648fb4c614d
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-01 17:43:43 +0200

    added the search functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,24 +6,12 @@ class PlacesController < ApplicationController
     @places = policy_scope(Place)
     # search bar - by sport
     @dropdown = SportType.all
-    if params[:query].present? && params[:sport].present?
-      sql_query = "places.name ILIKE :query AND sport_types.name ILIKE :sport"
-      @places = Place.joins(:sport_combination).where(sql_query, query: "%#{params[:query]}%", sport: "%#{params[:sport]}%")
-    
-    elsif params[:sport].present? && !params[:sport].blank?
-     # sql_query = "sport_types.name ILIKE :sport"
-     # @places = Place.joins(:sport_combination).where(sql_query, query: "%#{params[:query]}%", sport: "%#{params[:sport]}%") # , sport: "%#{params[:sport]}%"
-      # @places = Place.where(
-      #   "JOIN sport_combinations ON places.id = sport_combinations.place_id
-      #    JOIN sport_types ON sport_type.id = sport_combinations.sport_type_id
-      #    WHERE sport_types.name = :sport", sport: "#{params[:sport]}")
-
-
-     @places = Place.joins(:sport_types ).where('name = :sport', sport: "#{params[:sport]}")
-    # @places = Place.joins(:sport_combinations).left_joins(:ratings).where(ratings: { id: nil  })
-
-
-    elsif params[:query].present?
+    if params[:query].present? && params[:sport][:id].present?
+      @places = Place.joins(:sport_combinations).where("places.name ILIKE ? AND sport_combinations.sport_type_id = ?", "%#{params[:query]}%", params[:sport][:id].to_i)
+    elsif params[:sport].present? && !params[:sport][:id].blank?
+      sql_query = "sport_types.id = :sport"
+      @places = Place.joins(:sport_types).where(sql_query, sport: params[:sport][:id].to_i)
+    elsif params[:query].present? && params[:sport][:id].blank?
       sql_query = "name ILIKE :query"
       @places = Place.where(sql_query, query: "%#{params[:query]}%")
     else
