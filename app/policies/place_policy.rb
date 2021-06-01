1cea3f2a88dadbbd566cc702d3057653fb1a529a
Tea Filipovic <tea.filipovic0@gmail.com>
2021-06-01 14:28:47 +0200

    Places controller actions

--- app/policies/place_policy.rb
+++ app/policies/place_policy.rb
@@ -4,4 +4,24 @@ class PlacePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
 end
