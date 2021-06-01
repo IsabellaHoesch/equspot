84403d0c037da42140638d0453b486eff700e3a3
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-01 17:07:20 +0200

    updated seeds file. search bra in work

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,31 @@ class PlacesController < ApplicationController
 
   def index
     @places = policy_scope(Place)
+    # search bar - by sport
+    @dropdown = SportType.all
+    if params[:query].present? && params[:sport].present?
+      sql_query = "places.name ILIKE :query AND sport_types.name ILIKE :sport"
+      @places = Place.joins(:sport_combination).where(sql_query, query: "%#{params[:query]}%", sport: "%#{params[:sport]}%")
+    
+    elsif params[:sport].present? && !params[:sport].blank?
+     # sql_query = "sport_types.name ILIKE :sport"
+     # @places = Place.joins(:sport_combination).where(sql_query, query: "%#{params[:query]}%", sport: "%#{params[:sport]}%") # , sport: "%#{params[:sport]}%"
+      # @places = Place.where(
+      #   "JOIN sport_combinations ON places.id = sport_combinations.place_id
+      #    JOIN sport_types ON sport_type.id = sport_combinations.sport_type_id
+      #    WHERE sport_types.name = :sport", sport: "#{params[:sport]}")
+
+
+     @places = Place.joins(:sport_types ).where('name = :sport', sport: "#{params[:sport]}")
+    # @places = Place.joins(:sport_combinations).left_joins(:ratings).where(ratings: { id: nil  })
+
+
+    elsif params[:query].present?
+      sql_query = "name ILIKE :query"
+      @places = Place.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @places = Place.all
+    end
   end
 
   def show
