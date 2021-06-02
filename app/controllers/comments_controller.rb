9839b89ce1f3753727d61080e57295cdacdb3422
Armen Kaltak <arkouo@gmail.com>
2021-06-02 11:08:33 +0200

    Added the comment new and create routes to the router.rb. Defined in the comments controller the new function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def new
+    @user = current_user
+    @place = Place.find(params[:place_id])
+    # @comment.place = @place
+    # @comment.user = @user
+    @coment = Comment.new
+  end
+
+  def create
+
+  end
 end
