ce6189b4dad274b27c1f300a8050fcccf41a4196
Armen Kaltak <arkouo@gmail.com>
2021-06-02 11:56:22 +0200

    Added to the comments controller create function and authorizated the current user.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,22 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @place = Place.find(params[:place_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = @user
+    @comment.place = @place
+    authorize @comment
+    if @comment.save
+      redirect_to place_path(@comment), notice: "Added a comment."
+    else
+      render "places/show"
+    end
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :place_id)
   end
 end
