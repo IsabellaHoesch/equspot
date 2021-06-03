5e4a70759ce6678fe09928311eb28e9dd5a15fb0
andrea furthmair <a.furthmair@gmail.com>
2021-06-03 15:50:26 +0200

    added reviews show to place show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     resources :comments, only: [ :new, :create]
     resources :visits, only: [ :create ]
     resources :likes, only: [ :create ]
-    resources :reviews, only: [ :new, :create, :destroy ]
+    resources :reviews, only: [ :new, :create ]
   end
+  resources :reviews, only: [ :destroy ]
   resources :favourites, only: [ :index, :destroy ]
   resources :comments, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
