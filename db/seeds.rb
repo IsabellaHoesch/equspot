75cfda829ccfcdbe3e64aa2dfa28669d25458296
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-04 21:21:58 +0200

    wording improvement in seed-file

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,7 +57,7 @@ elements[n].each_with_index do |e, i|
   address =  e.text.strip
   address.sub! 'Munich', ' Munich'
   photo = "https://www.courtsoftheworld.com#{doc.css('div.owl-slide-parent a').map { |link| link['href'] }[i]}"
-  p = Place.create(name: "Basketball #{i}", address: address, description: "missing description", user: isa)
+  p = Place.create(name: "Basketball #{i}", address: address, description: "", user: isa)
   p.photos.attach(io: URI.open(photo), filename: "#{count}.png", content_type: 'image/png')
   p.sport_types.push(SportType.find_by(name: "Basketball"))
   p.save
@@ -87,7 +87,7 @@ calist[n].each do |cal|
   foto = doc.at_css('#links > a > img').attr('src')
   name = doc.search('#well > h1').text.strip
   address = doc.xpath('//*[@id="content-l-container"]/address/text()').text.strip
-  place = Place.new(name: "#{name}", address:address, description: "missing description", user: tea)
+  place = Place.new(name: "#{name}", address:address, description: "", user: tea)
   place.photos.attach(io: URI.open("#{foto}"), filename: "#{name}.png", content_type: 'image/png')
   place.sport_types.push(SportType.find_by(name: "Calisthetics"))
   place.save
@@ -97,17 +97,17 @@ puts "#{count} Calisthetics entries seeded"
 
 
 # create places: surf
-surf1 = Place.new(name: "Eisbachwelle", address: "Prinzregentenstraße, 80538 München", description: "Da das Surfen hier technisch anspruchsvoll und das Verletzungsrisiko wegen zementierten Störsteinen unter Wasser besonders groß ist, dürfen hier auf Basis einer Vereinbarung mit den städtischen Behörden nur sehr geübte Surfer mit mehreren Jahren Erfahrung ins Wasser.", user: isa)
+surf1 = Place.new(name: "Eisbachwelle", address: "Prinzregentenstraße, 80538 München", description: "Risk of injury is high due to cemented stones in water.", user: isa)
 surf1.photos.attach(io: URI.open("https://cdn.muenchen-p.de/.imaging/stk/responsive/image980/dms/fg-2018/outdoor-sport/isarsurferin-hp/document/isarsurferin-hp.jpg"), filename: 'surf1.png', content_type: 'image/png')
 surf1.sport_types.push(SportType.find_by(name: "Surf"))
 surf1.save
 
-surf2 = Place.new(name: "E2/Dianabadschwelle", address: "Himmelreichstraße, 80538, Munich", description: "Das Surfen an der Dianabadschwelle ist nicht legal und wird von der Stadt nur geduldet.", user: isa)
+surf2 = Place.new(name: "E2/Dianabadschwelle", address: "Himmelreichstraße, 80538, Munich", description: "Surfing here is officially not legal. Please surf quietly here.", user: isa)
 surf2.photos.attach(io: URI.open("https://www.igsm.info/wp-content/uploads/2015/08/Dianabadschwelle.jpg"), filename: 'surf2.png', content_type: 'image/png')
 surf2.sport_types.push(SportType.find_by(name: "Surf"))
 surf2.save
 
-surf3 = Place.new(name: "Floßlände", address: "Floßlände, Munich", description: "Die Welle läuft während der Floßsaison von Mai bis Oktober.", user: isa)
+surf3 = Place.new(name: "Floßlände", address: "Floßlände, Munich", description: "Great for beginners. You can surf here from MAy to October.", user: isa)
 surf3.photos.attach(io: URI.open("https://www.sueddeutsche.de/image/sz.1.4925326/1200x675?v=1591172107"), filename: 'surf3.png', content_type: 'image/png')
 surf3.sport_types.push(SportType.find_by(name: "Surf"))
 surf3.save
