792897a48e6bdf0f69905b1c1dee90cffd9969ce
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-07 16:02:48 +0200

    added charts, needs styling

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Find your favourite places", "Organize them", "Enjoy outdoor activities"],
-    typeSpeed: 50,
-    loop: true
-  });
+  if (document.querySelector("#banner-typed-text")) {
+    new Typed('#banner-typed-text', {
+      strings: ["Find your favourite places", "Organize them", "Enjoy outdoor activities"],
+      typeSpeed: 50,
+      loop: true
+    });
+  };
 }
 
 export { loadDynamicBannerText };
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'date'
 
 num = 5
 n = 0..num
@@ -29,6 +30,30 @@ isa = User.create(email: "isa@hotmail.com", password: "1234567", first_name: "Is
 tea = User.create(email: "tea@hotmail.com", password: "1234567", first_name: "Tea", last_name: "Filipovic")
 andrea = User.create(email: "andrea@hotmail.com", password: "1234567", first_name: "Andrea", last_name: "Furthmair")
 
+# create places: surf
+surf1 = Place.new(name: "Eisbachwelle", address: "Prinzregentenstraße, 80538 München", description: "Risk of injury is high due to cemented rocks in water.", user: isa)
+surf1.photos.attach(io: URI.open("https://cdn.muenchen-p.de/.imaging/stk/responsive/image980/dms/fg-2018/outdoor-sport/isarsurferin-hp/document/isarsurferin-hp.jpg"), filename: 'surf1.png', content_type: 'image/png')
+surf1.sport_types.push(SportType.find_by(name: "Surf"))
+surf1.save
+month = 2
+10.times do
+  rand(1..15).times do
+    Visit.create(user: isa, place: surf1, created_at: DateTime.new(2020,month,3,4,5,6))
+  end
+  month += 1
+end
+
+surf2 = Place.new(name: "E2/Dianabadschwelle", address: "Himmelreichstraße, 80538, Munich", description: "Surfing here is officially not legal. Please surf quietly here.", user: isa)
+surf2.photos.attach(io: URI.open("https://www.igsm.info/wp-content/uploads/2015/08/Dianabadschwelle.jpg"), filename: 'surf2.png', content_type: 'image/png')
+surf2.sport_types.push(SportType.find_by(name: "Surf"))
+surf2.save
+
+surf3 = Place.new(name: "Floßlände", address: "Floßlände, Munich", description: "Great for beginners. You can surf here from May to October.", user: isa)
+surf3.photos.attach(io: URI.open("https://www.sueddeutsche.de/image/sz.1.4925326/1200x675?v=1591172107"), filename: 'surf3.png', content_type: 'image/png')
+surf3.sport_types.push(SportType.find_by(name: "Surf"))
+surf3.save
+puts "3 Surf entries seeded"
+
 # calisthetics
 count = 0
 CSV.foreach("db/calisthetics.csv") do |row|
@@ -36,15 +61,17 @@ CSV.foreach("db/calisthetics.csv") do |row|
     foto = "https://www.hall-of-sports.de/wp-content/uploads/2018/08/E3A1715-1030x686.jpg" #row[0]
     name = row[1]
     address = row[2]
-    puts "foto: #{foto}"
-    puts "name: #{name}"
-    puts "address: #{address}"
-
     place = Place.new(name: name, address:address, description: "Missing description", user: tea)
     place.photos.attach(io: URI.open("#{foto}"), filename: "#{name}.jpg", content_type: 'image/png')
     place.sport_types.push(SportType.find_by(name: "Calisthetics"))
     place.save
     count += 1
+    # add some visits
+    [arko, isa, tea, andrea].sample do |user|
+      rand(0..2).times do
+        Visit.create(user: user, place: place)
+      end
+    end
   end
 end
 puts "#{count} Calisthetics entries seeded"
@@ -63,6 +90,12 @@ document.root.xpath('marker')[n].each do |pp|
   place.sport_types.push(SportType.find_by(name: "Ping-Pong"))
   place.save
   count += 1
+  # add some visits
+  [arko, isa, tea, andrea].sample do |user|
+    rand(0..2).times do
+      Visit.create(user: user, place: place)
+    end
+  end
 end
 puts "#{count} Ping-Pong entries seeded"
 
@@ -82,27 +115,16 @@ elements[n].each_with_index do |e, i|
   p.sport_types.push(SportType.find_by(name: "Basketball"))
   p.save
   count += 1
+  # add some visits
+  [arko, tea, andrea].sample do |user|
+    rand(0..2).times do
+      Visit.new(user: user, place: place)
+    end
+  end
 end
 puts "#{count} Basketball entries seeded"
 
 
-# create places: surf
-surf1 = Place.new(name: "Eisbachwelle", address: "Prinzregentenstraße, 80538 München", description: "Risk of injury is high due to cemented rocks in water.", user: isa)
-surf1.photos.attach(io: URI.open("https://cdn.muenchen-p.de/.imaging/stk/responsive/image980/dms/fg-2018/outdoor-sport/isarsurferin-hp/document/isarsurferin-hp.jpg"), filename: 'surf1.png', content_type: 'image/png')
-surf1.sport_types.push(SportType.find_by(name: "Surf"))
-surf1.save
-
-surf2 = Place.new(name: "E2/Dianabadschwelle", address: "Himmelreichstraße, 80538, Munich", description: "Surfing here is officially not legal. Please surf quietly here.", user: isa)
-surf2.photos.attach(io: URI.open("https://www.igsm.info/wp-content/uploads/2015/08/Dianabadschwelle.jpg"), filename: 'surf2.png', content_type: 'image/png')
-surf2.sport_types.push(SportType.find_by(name: "Surf"))
-surf2.save
-
-surf3 = Place.new(name: "Floßlände", address: "Floßlände, Munich", description: "Great for beginners. You can surf here from May to October.", user: isa)
-surf3.photos.attach(io: URI.open("https://www.sueddeutsche.de/image/sz.1.4925326/1200x675?v=1591172107"), filename: 'surf3.png', content_type: 'image/png')
-surf3.sport_types.push(SportType.find_by(name: "Surf"))
-surf3.save
-
-puts "3 Surf entries seeded"
 
 
-#add reviews, signups, messages
+
