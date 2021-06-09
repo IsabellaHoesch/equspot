c2bcaf414e7af2cc80058e923f0760f879250716
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-09 11:14:51 +0200

    changed fav cycle to cycle once only then stop

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,9 +3,9 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (document.querySelector("#banner-typed-text")) {
     new Typed('#banner-typed-text', {
-      strings: ["Find your favourite places", "Organize them", "Enjoy outdoor activities"],
+      strings: ["Connect with other players and", "meet at your favourite places!"],
       typeSpeed: 50,
-      loop: true
+      loop: false
     });
   };
 }
