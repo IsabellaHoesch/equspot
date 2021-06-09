35a9555fd7b81708a1493c2dac931f45c23d27b9
Armen Kaltak <arkouo@gmail.com>
2021-06-09 10:39:04 +0200

    Fixed the bug with seeding the files. The seeding wasnt saving the place so I added explanation mark to all create and save methods. And in the end the description was always empty. Now its displaying the name of the place as a descriotion

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,12 +6,9 @@ class Place < ApplicationRecord
   has_many :sport_combinations, dependent: :destroy
   has_many :sport_types, through: :sport_combinations
   has_many_attached :photos
-  validates :name, presence: true, uniqueness: true, length: { maximum: 30, message: "no longer than 30 characters." }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50, message: "no longer than 50 characters." }
   validates :description, presence: true, length: { maximum: 250, message: "no longer than 250 characters." }
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  def blank_stars
-    5 - rating
-  end
 end
