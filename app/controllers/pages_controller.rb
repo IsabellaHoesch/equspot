e38d8018fadbdd42402da59ebcd2aed4761297f4
Isabella Hoesch <isabella.hoesch@outlook.com>
2021-06-09 15:59:02 +0200

    more wording changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
     # @sporttypes = SportType.all
     @visit_this_month = @user.visits.where("created_at > ?", 4.weeks.ago).count 
     @visit_past_month = @user.visits.where('created_at BETWEEN ? AND ?', 8.weeks.ago, 4.weeks.ago).count 
-    @percentage_change_month = (@visit_past_month.fdiv(@visit_this_month) / @visit_past_month * 100).to_f.nan? ? 0 : (@visit_past_month.fdiv(@visit_this_month) / @visit_past_month * 100).round(1)
+    @percentage_change_month = (@visit_past_month.fdiv(@visit_this_month) / @visit_past_month * 100).to_f.nan? ? 0 : (@visit_past_month.fdiv(@visit_this_month) / @visit_past_month * 100).round(0)
     @motivational_msg = @percentage_change_month.positive? ? "Great job!" : "#{@user.first_name}, you´re slacking! Get going!"
   end
 
